796c55db967b15ea85dec9ab4c20c27007bfd190
Franz Asperger <f.asperger@gmail.com>
2023-01-16 10:34:41 -0600

    complete project upload

---
+++ script.js
@@ -0,0 +1,68 @@
+const getComputerChoice = () => {
+  let choice = Math.floor(Math.random() * 3 + 1);
+  if (choice == 1) {
+    choice = "rock";
+  } else if (choice == 2) {
+    choice = "paper";
+  } else {
+    choice = "scissors";
+  }
+  return choice;
+};
+
+let playRound = (playerSelection, computerSelection) => {
+  playerSelection = playerSelection.toLowerCase();
+
+  if (playerSelection === computerSelection) {
+    return "It's a draw!";
+  } else if (playerSelection === "rock" && computerSelection === "paper") {
+    return "Computer Wins!";
+  } else if (playerSelection === "rock" && computerSelection === "scissors") {
+    return "Player wins!";
+  } else if (playerSelection === "paper" && computerSelection === "rock") {
+    return "Player wins!";
+  } else if (playerSelection === "paper" && computerSelection === "scissors") {
+    return "Computer Wins!";
+  } else if (playerSelection === "scissors" && computerSelection === "rock") {
+    return "Computer Wins!";
+  } else if (playerSelection === "scissors" && computerSelection === "paper") {
+    return "Player wins!";
+  } else {
+    return "Please enter a valid input";
+  }
+};
+
+let game = () => {
+  const computerSelection = getComputerChoice();
+  const playerSelection = prompt("Please choose rock, paper or scissors");
+  return playRound(playerSelection, computerSelection);
+};
+
+let myScore = 0;
+let computerScore = 0;
+for (i = 0; i < 5; i++) {
+  let playedGame = game();
+  if (playedGame == "Computer Wins!") {
+    computerScore++;
+  } else if (playedGame == "Player wins!") {
+    myScore++;
+  } else {
+    console.log("Draw!");
+  }
+  console.log(`Your Score is: ${myScore}`);
+  console.log(`Computer's score is: ${computerScore}`);
+}
+
+console.log(`
+  Final score:
+  Player: ${myScore}.
+  Computer: ${computerScore}.
+  `);
+
+if (computerScore > myScore) {
+  console.log("Computer Wins!");
+} else if (computerScore < myScore) {
+  console.log("Player Wins!");
+} else {
+  console.log("Draw!");
+}
